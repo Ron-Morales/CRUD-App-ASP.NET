c1d43823bfccc9d00b9cee34c755995bfe014ebc
Ronald <ronaldmorales777@gmail.com>
2020-07-21 23:22:15 -0700

    Add project files.

---
+++ BasicCrudOp(BookListProject)/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BasicCrudOp_BookListProject_.Model
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
